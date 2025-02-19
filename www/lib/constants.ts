d63a4622f258efc59a2e84a738148f5fcfd87e6c
Cyndra-project <junaidgohan123@gmail.com>
2025-02-19 15:39:37 +0400

    feat: klyra.rs 🚀 (#45)
    * copy www from synth repo
    
    * change domain from getsynth.com to klyra.rs
    
    * change colors, logo and app name
    
    * fix more content
    
    * update all packages
    
    * update Hero component
    
    * fix ssr issue
    
    * update SITE_TITLE and SITE_DESCRIPTION
    
    * clean up
    
    * open discord link in new tab
    
    * add clipboard icon to code snippet
    
    * move footer columns to right
    
    * update logo, favicon, texts, styles
    
    * update styles of hero section
    
    * better spacing in hero section
    
    * add alpha label
    
    * add deploy job for www
    
    * remove unused file
    
    * add mixpanel, remove unused code, etc
    
    * update hero styles, hide footer, add announcement
    
    * updated header and subtitle copy
    
    * header tweak
    
    * rename to klyra
    
    * fix crate name

---
+++ www/lib/constants.ts
@@ -0,0 +1,11 @@
+export const APP_NAME = 'Klyra'
+
+export const SITE_TITLE = 'Deploy Rust apps with a single Cargo command'
+
+export const SITE_DESCRIPTION = 'Klyra is a crate that uses traits and annotations to configure your backend deployment - including databases.'
+
+export const SITE_URL = 'https://klyra.rs/'
+
+export const TWITTER_HANDLE = '@getsynth'
+
+export const MIXPANEL_TOKEN = 'ea13713300ea92e06107f27762512e57'
