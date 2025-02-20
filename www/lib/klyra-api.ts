068d6454bb84675329eaeeec23ff90029205da68
Cyndra-project <junaidgohan123@gmail.com>
2025-02-21 03:39:25 +0400

    fix: lock users api and allow re-issuing keys

--- www/lib/klyra-api.ts
+++ www/lib/klyra-api.ts
@@ -2,7 +2,10 @@ import fetch from 'node-fetch';
 
 export async function getApiKey(username: string): Promise<string> {
   const res = await fetch(`${process.env.klyra_API_BASE_URL}/users/${username}`, {
-    method: 'POST'
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.klyra_ADMIN_SECRET}`
+    }
   })
 
   if (res.ok) {
