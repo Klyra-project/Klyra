177cb4f124844f58d57b492135230942482f1b18
Cyndra-project <junaidgohan123@gmail.com>
2025-02-23 07:52:41 +0400

    updated home page (#102)

--- www/lib/constants.ts
+++ www/lib/constants.ts
@@ -1,13 +1,18 @@
-export const APP_NAME = 'klyra'
+export const APP_NAME = "klyra";
 
-export const SITE_TITLE = 'Deploy Rust apps with a single Cargo command'
+export const SITE_TITLE = "Deploy Rust apps with a single Cargo command";
 
-export const SITE_DESCRIPTION = 'Klyra is a crate that uses traits and annotations to configure your backend deployment - including databases.'
+export const SITE_DESCRIPTION =
+  "Klyra is a crate that uses traits and annotations to configure your backend deployment - including databases.";
 
-export const SITE_URL = 'https://klyra.rs/'
+export const SITE_URL = "https://klyra.rs/";
 
-export const TWITTER_HANDLE = '@getsynth'
+export const TWITTER_HANDLE = "@klyra_dev";
 
-export const MIXPANEL_TOKEN = 'ea13713300ea92e06107f27762512e57'
+export const MIXPANEL_TOKEN = "ea13713300ea92e06107f27762512e57";
 
-export const DISCORD_URL = 'https://discord.gg/H33rRDTm3p'
+export const GITHUB_URL = "https://github.com/getsynth/klyra";
+export const DISCORD_URL = "https://discord.gg/H33rRDTm3p";
+export const TWITTER_URL = "https://twitter.com/klyra_dev";
+export const klyra_DOCS_URL =
+  "https://docs.rs/klyra-service/latest/klyra_service/";
--- www/lib/klyra-api.ts
+++ www/lib/klyra-api.ts
@@ -1,16 +1,19 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 export async function getApiKey(username: string): Promise<string> {
-  const res = await fetch(`${process.env.klyra_API_BASE_URL}/users/${username}`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${process.env.klyra_ADMIN_SECRET}`
+  const res = await fetch(
+    `${process.env.klyra_API_BASE_URL}/users/${username}`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.klyra_ADMIN_SECRET}`,
+      },
     }
-  })
+  );
 
   if (res.ok) {
-    return res.text()
+    return res.text();
   } else {
-    throw new Error('could not get api key.')
+    throw new Error("could not get api key.");
   }
 }
