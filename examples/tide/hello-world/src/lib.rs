f8a200f203decfa330a6aed71429089afc452353
Cyndra-project <junaidgohan123@gmail.com>
2025-03-01 22:14:30 -0300

    add support to tide framework (#172)
    * feat: add tide as deps on service crate
    
    * feat: implement service to tide framework
    
    * feat: add tide example
    
    * feat: add tide example on users.toml file
    
    * test: add e2e tests to tide
    
    * ref: remove pre build tide server config
    
    
    * style: cargo fmt
    
    * feat: remove SyncWrap crate from tide example
    
    * style: cargo fmt
    
    * ref: just using SimpleService impl
    
    * ref: remove sync wrapper from tide example
    
    
    * feat: update impl from tide SimpleServer
    
    * fix: remove comma from return

---
+++ examples/tide/hello-world/src/lib.rs
@@ -0,0 +1,11 @@
+
+#[klyra_service::main]
+async fn tide() -> Result<tide::Server<()>, klyra_service::Error> {
+    let mut app = tide::new();
+    app.with(tide::log::LogMiddleware::new());
+
+    app.at("/hello")
+        .get(|_| async { Ok("Hello, world!") });
+
+    Ok(app)
+}
