5c8a81b12cc6dfa8a929b8e41ad59b43919b4d2f
Cyndra-project <junaidgohan123@gmail.com>
2025-03-11 06:41:56 -0400

    deps(clap): migrate uses of structopt to clap in cargo-klyra (#257)
    * deps(clap): migrate uses of structopt to clap in cargo-klyra
    
    * use doc comments for help text

--- cargo-klyra/src/args.rs
+++ cargo-klyra/src/args.rs
@@ -1,101 +1,104 @@
 use std::{
-    ffi::{OsStr, OsString},
+    ffi::OsStr,
     fs::{canonicalize, create_dir_all},
+    io::{self, ErrorKind},
     path::PathBuf,
 };
 
+use clap::Parser;
 use klyra_common::project::ProjectName;
-use structopt::StructOpt;
 
-#[derive(StructOpt)]
-#[structopt(
+#[derive(Parser)]
+#[clap(
     // Cargo passes in the subcommand name to the invoked executable. Use a
     // hidden, optional positional argument to deal with it.
-    arg(structopt::clap::Arg::with_name("dummy")
+    arg(clap::Arg::with_name("dummy")
         .possible_value("klyra")
         .required(false)
         .hidden(true))
 )]
 pub struct Args {
-    #[structopt(
-        long,
-        about = "allows targeting a custom deloyed instance for this command only",
-        env = "klyra_API"
-    )]
-    /// Run this command against the api at the supplied url
+    /// run this command against the api at the supplied url
+    /// (allows targeting a custom deployed instance for this command only)
+    #[clap(long, env = "klyra_API")]
     pub api_url: Option<String>,
-    #[structopt(flatten)]
+    #[clap(flatten)]
     pub project_args: ProjectArgs,
-    #[structopt(subcommand)]
+    #[clap(subcommand)]
     pub cmd: Command,
 }
 
 // Common args for subcommands that deal with projects.
-#[derive(StructOpt, Debug)]
+#[derive(Parser, Debug)]
 pub struct ProjectArgs {
-    #[structopt(
+    /// Specify the working directory
+    #[clap(
         global = true,
         long,
         parse(try_from_os_str = parse_path),
         default_value = ".",
     )]
-    /// Specify the working directory
     pub working_directory: PathBuf,
-    #[structopt(global = true, long)]
     /// Specify the name of the project (overrides crate name)
+    #[clap(global = true, long)]
     pub name: Option<ProjectName>,
 }
 
-#[derive(StructOpt)]
+#[derive(Parser)]
 pub enum Command {
-    #[structopt(about = "deploy a klyra project")]
+    /// deploy a klyra project
     Deploy(DeployArgs),
-    #[structopt(about = "create a new klyra project")]
+    /// create a new klyra project
     Init(InitArgs),
-    #[structopt(about = "view the status of a klyra project")]
+    /// view the status of a klyra project
     Status,
-    #[structopt(about = "view the logs of a klyra project")]
+    /// view the logs of a klyra project
     Logs,
-    #[structopt(about = "delete the latest deployment for a klyra project")]
+    /// delete the latest deployment for a klyra project
     Delete,
-    #[structopt(about = "create user credentials for the klyra platform")]
+    /// create user credentials for the klyra platform
     Auth(AuthArgs),
-    #[structopt(about = "login to the klyra platform")]
+    /// login to the klyra platform
     Login(LoginArgs),
-    #[structopt(about = "run a klyra project locally")]
+    /// run a klyra project locally
     Run(RunArgs),
 }
 
-#[derive(StructOpt)]
+#[derive(Parser)]
 pub struct LoginArgs {
-    #[structopt(long, about = "api key for the klyra platform")]
+    /// api key for the klyra platform
+    #[clap(long)]
     pub api_key: Option<String>,
 }
 
-#[derive(StructOpt)]
+#[derive(Parser)]
 pub struct AuthArgs {
-    #[structopt(about = "the desired username for the klyra platform")]
+    /// the desired username for the klyra platform
+    #[clap()]
     pub username: String,
 }
 
-#[derive(StructOpt)]
+#[derive(Parser)]
 pub struct DeployArgs {
-    #[structopt(long, about = "allow dirty working directories to be packaged")]
+    /// allow dirty working directories to be packaged
+    #[clap(long)]
     pub allow_dirty: bool,
-    #[structopt(long, about = "allows pre-deploy tests to be skipped")]
+    /// allows pre-deploy tests to be skipped
+    #[clap(long)]
     pub no_test: bool,
 }
 
-#[derive(StructOpt, Debug)]
+#[derive(Parser, Debug)]
 pub struct RunArgs {
-    #[structopt(long, about = "port to start service on", default_value = "8000")]
+    /// port to start service on
+    #[clap(long, default_value = "8000")]
     pub port: u16,
 }
 
-#[derive(StructOpt)]
+#[derive(Parser)]
 pub struct InitArgs {
-    #[structopt(
-        about = "the path to initialize a new klyra project",
+    /// the path to initialize a new klyra project
+    #[clap(
         parse(try_from_os_str = parse_init_path),
         default_value = ".",
     )]
@@ -103,14 +106,19 @@ pub struct InitArgs {
 }
 
 // Helper function to parse and return the absolute path
-fn parse_path(path: &OsStr) -> Result<PathBuf, OsString> {
-    canonicalize(path).map_err(|e| format!("could not turn {path:?} into a real path: {e}").into())
+fn parse_path(path: &OsStr) -> Result<PathBuf, io::Error> {
+    canonicalize(path).map_err(|e| {
+        io::Error::new(
+            ErrorKind::InvalidInput,
+            format!("could not turn {path:?} into a real path: {e}"),
+        )
+    })
 }
 
 // Helper function to parse, create if not exists, and return the absolute path
-fn parse_init_path(path: &OsStr) -> Result<PathBuf, OsString> {
+fn parse_init_path(path: &OsStr) -> Result<PathBuf, io::Error> {
     // Create the directory if does not exist
-    create_dir_all(path).expect("could not find or create a directory with the given path");
+    create_dir_all(path)?;
 
     parse_path(path)
 }
