f3527c0c22b97e69f1411ab4454a68b537a38437
Cyndra-project <junaidgohan123@gmail.com>
2025-03-13 16:37:00 +0200

    feat: deployer build logs (#265)
    * refactor: enrich logs
    
    * feat: connect build logs to persistence
    
    * feat: build logs web socket
    
    * refactor: From impl for BuildLog
    
    * refactor: names and comments
    
    * refactor: check for done
    
    * refactor: comment
    
    * refactor: to_build_log
    
    * refactor: unneeded future in spawn
    
    * tests: for get_build_logs cases
    
    * refactor: handle unwrap in ws hangler
    
    * refactor: don't panic in log distribution thread
    
    * refactor: clippy suggestion

--- deployer/tests/resources/tests-pass/src/lib.rs
+++ deployer/tests/resources/tests-pass/src/lib.rs
@@ -15,7 +15,7 @@ async fn rocket() -> klyra_service::KlyraRocket {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn this_fails() {
+    fn this_passes() {
         assert_eq!(super::index(), "Hello, world!");
     }
 }
