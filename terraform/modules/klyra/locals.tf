4deca819ab2bfee82eee4cbc4ed0d01cdeb32f20
Cyndra-project <junaidgohan123@gmail.com>
2025-03-21 01:37:22 +0100

    infra: migrate the builder to x86

--- terraform/modules/klyra/locals.tf
+++ terraform/modules/klyra/locals.tf
@@ -3,6 +3,6 @@ data "aws_caller_identity" "current" {}
 locals {
   account_id               = data.aws_caller_identity.current.account_id
   data_dir                 = "/opt/klyra"
-  docker_backend_image     = "public.ecr.aws/klyra/backend"
+  docker_backend_image     = "public.ecr.aws/klyra/api"
   docker_provisioner_image = "public.ecr.aws/klyra/provisioner"
 }
