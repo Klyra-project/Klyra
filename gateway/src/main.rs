1edde8d67026c268b5fe586f79f4c166c2f3615d
Cyndra-project <junaidgohan123@gmail.com>
2025-03-24 13:50:41 +0200

    feat: gateway init (#363)
    * refactor: split off start command
    
    * refactor: move migration to main
    
    * feat: init command
    
    * refactor: clap 4 convention
    
    * refactor: StartArgs

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -1,14 +1,20 @@
 use clap::Parser;
 use futures::prelude::*;
-use klyra_gateway::api::make_api;
-use klyra_gateway::args::Args;
+use klyra_gateway::args::{Args, Commands, InitArgs};
+use klyra_gateway::auth::Key;
 use klyra_gateway::proxy::make_proxy;
 use klyra_gateway::service::GatewayService;
 use klyra_gateway::worker::Worker;
+use klyra_gateway::{api::make_api, args::StartArgs};
+use sqlx::migrate::{MigrateDatabase, Migrator};
+use sqlx::{query, Sqlite, SqlitePool};
 use std::io;
+use std::path::Path;
 use std::sync::Arc;
 use tracing::{error, info, trace};
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+
+static MIGRATIONS: Migrator = sqlx::migrate!("./migrations");
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -32,7 +38,28 @@ async fn main() -> io::Result<()> {
         .with(opentelemetry)
         .init();
 
-    let gateway = Arc::new(GatewayService::init(args.clone()).await);
+    if !Path::new(&args.state).exists() {
+        Sqlite::create_database(&args.state).await.unwrap();
+    }
+
+    info!(
+        "state db: {}",
+        std::fs::canonicalize(&args.state)
+            .unwrap()
+            .to_string_lossy()
+    );
+    let db = SqlitePool::connect(&args.state).await.unwrap();
+
+    MIGRATIONS.run(&db).await.unwrap();
+
+    match args.command {
+        Commands::Start(start_args) => start(db, start_args).await,
+        Commands::Init(init_args) => init(db, init_args).await,
+    }
+}
+
+async fn start(db: SqlitePool, args: StartArgs) -> io::Result<()> {
+    let gateway = Arc::new(GatewayService::init(args.clone(), db).await);
 
     let worker = Worker::new(Arc::clone(&gateway));
     gateway.set_sender(Some(worker.sender())).await.unwrap();
@@ -59,5 +86,22 @@ async fn main() -> io::Result<()> {
 
     let _ = tokio::join!(worker_handle, api_handle, proxy_handle);
 
+    Ok(())
+}
+
+async fn init(db: SqlitePool, args: InitArgs) -> io::Result<()> {
+    let key = match args.key {
+        Some(key) => key,
+        None => Key::new_random(),
+    };
+
+    query("INSERT INTO accounts (account_name, key, super_user) VALUES (?1, ?2, 1)")
+        .bind(&args.name)
+        .bind(&key)
+        .execute(&db)
+        .await
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+    println!("`{}` created as super user with key: {key}", args.name);
     Ok(())
 }
