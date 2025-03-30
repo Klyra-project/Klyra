1a50a4603c820087c1f0918999291b0d5f19e1cb
Cyndra-project <junaidgohan123@gmail.com>
2025-03-30 09:52:44 +0200

    feat: create a new admin cli binary crate (#462)
    * feat: init klyra-admin
    
    * feat: add args
    
    * feat: simple client
    
    * feat: read api_key
    
    * refactor: hook it all together
    
    * refactor: switch to post

---
+++ admin/src/config.rs
@@ -0,0 +1,15 @@
+use std::{fs, path::PathBuf};
+
+pub fn get_api_key() -> String {
+    let data = fs::read_to_string(config_path()).expect("klyra config file to exist");
+    let toml: toml::Value = toml::from_str(&data).expect("to parse klyra config file");
+
+    toml["api_key"].to_string()
+}
+
+fn config_path() -> PathBuf {
+    dirs::config_dir()
+        .expect("system to have a config path")
+        .join("klyra")
+        .join("config.toml")
+}
