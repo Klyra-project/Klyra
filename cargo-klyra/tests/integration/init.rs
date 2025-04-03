2548614ec0ae2de4acccf4f427adfdb73bc6b2dd
Cyndra-project <junaidgohan123@gmail.com>
2025-04-03 23:12:01 +0100

    feat: interactive project initialization (#498)

--- cargo-klyra/tests/integration/init.rs
+++ cargo-klyra/tests/integration/init.rs
@@ -1,101 +1,177 @@
-use std::{
-    fs::read_to_string,
-    path::{Path, PathBuf},
-};
+use std::fs::read_to_string;
+use std::path::Path;
+use std::process::Command;
 
-use cargo_klyra::{Args, Command, CommandOutcome, InitArgs, ProjectArgs, Klyra};
+use cargo_klyra::{Args, Klyra};
+use clap::Parser;
 use indoc::indoc;
 use tempfile::Builder;
 
-/// creates a `cargo-klyra` init instance with some reasonable defaults set.
-async fn cargo_klyra_init(path: PathBuf) -> anyhow::Result<CommandOutcome> {
-    let working_directory = Path::new(".").to_path_buf();
-
-    Klyra::new()
-        .unwrap()
-        .run(Args {
-            api_url: Some("http://klyra.invalid:80".to_string()),
-            project_args: ProjectArgs {
-                working_directory,
-                name: None,
-            },
-            cmd: Command::Init(InitArgs {
-                actix_web: false,
-                axum: false,
-                rocket: false,
-                tide: false,
-                tower: false,
-                poem: false,
-                salvo: false,
-                serenity: false,
-                warp: false,
-                thruster: false,
-                path,
-            }),
-        })
-        .await
-}
-
-/// creates a `cargo-klyra` init instance for initializing the `rocket` framework
-async fn cargo_klyra_init_framework(path: PathBuf) -> anyhow::Result<CommandOutcome> {
-    let working_directory = Path::new(".").to_path_buf();
-
-    Klyra::new()
-        .unwrap()
-        .run(Args {
-            api_url: Some("http://klyra.invalid:80".to_string()),
-            project_args: ProjectArgs {
-                working_directory,
-                name: None,
-            },
-            cmd: Command::Init(InitArgs {
-                actix_web: false,
-                axum: false,
-                rocket: true,
-                tide: false,
-                tower: false,
-                poem: false,
-                salvo: false,
-                serenity: false,
-                warp: false,
-                thruster: false,
-                path,
-            }),
-        })
-        .await
-}
-
 #[tokio::test]
-async fn basic_init() {
+async fn non_interactive_basic_init() {
     let temp_dir = Builder::new().prefix("basic-init").tempdir().unwrap();
     let temp_dir_path = temp_dir.path().to_owned();
 
-    cargo_klyra_init(temp_dir_path.clone()).await.unwrap();
-    let cargo_toml = read_to_string(temp_dir_path.join("Cargo.toml")).unwrap();
+    let args = Args::parse_from([
+        "cargo-klyra",
+        "--api-url",
+        "http://klyra.invalid:80",
+        "init",
+        "--api-key",
+        "fake-api-key",
+        "--name",
+        "my-project",
+        "--no-framework",
+        temp_dir_path.to_str().unwrap(),
+    ]);
+    Klyra::new().unwrap().run(args).await.unwrap();
 
+    let cargo_toml = read_to_string(temp_dir_path.join("Cargo.toml")).unwrap();
     // Expected: name = "basic-initRANDOM_CHARS"
     assert!(cargo_toml.contains("name = \"basic-init"));
     assert!(cargo_toml.contains("klyra-service = { version = "));
 }
 
 #[tokio::test]
-async fn framework_init() {
+async fn non_interactive_rocket_init() {
     let temp_dir = Builder::new().prefix("rocket-init").tempdir().unwrap();
     let temp_dir_path = temp_dir.path().to_owned();
 
-    cargo_klyra_init_framework(temp_dir_path.clone())
-        .await
-        .unwrap();
+    let args = Args::parse_from([
+        "cargo-klyra",
+        "--api-url",
+        "http://klyra.invalid:80",
+        "init",
+        "--api-key",
+        "fake-api-key",
+        "--name",
+        "my-project",
+        "--rocket",
+        temp_dir_path.to_str().unwrap(),
+    ]);
+    Klyra::new().unwrap().run(args).await.unwrap();
 
-    let cargo_toml = read_to_string(temp_dir_path.join("Cargo.toml")).unwrap();
+    assert_valid_rocket_project(temp_dir_path.as_path(), "rocket-init");
+}
 
-    // Expected: name = "rocket-initRANDOM_CHARS"
-    assert!(cargo_toml.contains("name = \"rocket-init"));
+#[test]
+fn interactive_rocket_init() -> Result<(), Box<dyn std::error::Error>> {
+    let temp_dir = Builder::new().prefix("rocket-init").tempdir().unwrap();
+    let temp_dir_path = temp_dir.path().to_owned();
+
+    let bin_path = assert_cmd::cargo::cargo_bin("cargo-klyra");
+    let mut command = Command::new(bin_path);
+    command.args([
+        "--api-url",
+        "http://klyra.invalid:80",
+        "init",
+        "--api-key",
+        "fake-api-key",
+    ]);
+    let mut session = rexpect::session::spawn_command(command, Some(2000))?;
+
+    session.exp_string(
+        "How do you want to name your project? It will be hosted at ${project_name}.klyraapp.rs.",
+    )?;
+    session.exp_string("Project name")?;
+    session.send_line("my-project")?;
+    session.exp_string("Where should we create this project?")?;
+    session.exp_string("Directory")?;
+    session.send_line(temp_dir_path.to_str().unwrap())?;
+    session.exp_string(
+        "Klyra works with a range of web frameworks. Which one do you want to use?",
+    )?;
+    // Partial input should be enough to match "rocket"
+    session.send_line("roc")?;
+    session.exp_string("Do you want to create the project environment on Klyra?")?;
+    session.send("y")?;
+    session.flush()?;
+    session.exp_string("yes")?;
+
+    assert_valid_rocket_project(temp_dir_path.as_path(), "rocket-init");
+
+    Ok(())
+}
+
+#[test]
+fn interactive_rocket_init_dont_prompt_framework() -> Result<(), Box<dyn std::error::Error>> {
+    let temp_dir = Builder::new().prefix("rocket-init").tempdir().unwrap();
+    let temp_dir_path = temp_dir.path().to_owned();
+
+    let bin_path = assert_cmd::cargo::cargo_bin("cargo-klyra");
+    let mut command = Command::new(bin_path);
+    command.args([
+        "--api-url",
+        "http://klyra.invalid:80",
+        "init",
+        "--api-key",
+        "fake-api-key",
+        "--rocket",
+    ]);
+    let mut session = rexpect::session::spawn_command(command, Some(2000))?;
+
+    session.exp_string(
+        "How do you want to name your project? It will be hosted at ${project_name}.klyraapp.rs.",
+    )?;
+    session.exp_string("Project name")?;
+    session.send_line("my-project")?;
+    session.exp_string("Where should we create this project?")?;
+    session.exp_string("Directory")?;
+    session.send_line(temp_dir_path.to_str().unwrap())?;
+    session.exp_string("Do you want to create the project environment on Klyra?")?;
+    session.send("y")?;
+    session.flush()?;
+    session.exp_string("yes")?;
+
+    assert_valid_rocket_project(temp_dir_path.as_path(), "rocket-init");
+
+    Ok(())
+}
+
+#[test]
+fn interactive_rocket_init_dont_prompt_name() -> Result<(), Box<dyn std::error::Error>> {
+    let temp_dir = Builder::new().prefix("rocket-init").tempdir().unwrap();
+    let temp_dir_path = temp_dir.path().to_owned();
+
+    let bin_path = assert_cmd::cargo::cargo_bin("cargo-klyra");
+    let mut command = Command::new(bin_path);
+    command.args([
+        "--api-url",
+        "http://klyra.invalid:80",
+        "init",
+        "--api-key",
+        "fake-api-key",
+        "--name",
+        "my-project",
+    ]);
+    let mut session = rexpect::session::spawn_command(command, Some(2000))?;
+
+    session.exp_string("Where should we create this project?")?;
+    session.exp_string("Directory")?;
+    session.send_line(temp_dir_path.to_str().unwrap())?;
+    session.exp_string(
+        "Klyra works with a range of web frameworks. Which one do you want to use?",
+    )?;
+    // Partial input should be enough to match "rocket"
+    session.send_line("roc")?;
+    session.exp_string("Do you want to create the project environment on Klyra?")?;
+    session.send("y")?;
+    session.flush()?;
+    session.exp_string("yes")?;
+
+    assert_valid_rocket_project(temp_dir_path.as_path(), "rocket-init");
+
+    Ok(())
+}
+
+fn assert_valid_rocket_project(path: &Path, name_prefix: &str) {
+    let cargo_toml = read_to_string(path.join("Cargo.toml")).unwrap();
+    assert!(cargo_toml.contains(&format!("name = \"{name_prefix}")));
     assert!(cargo_toml.contains("klyra-service = { version = "));
     assert!(cargo_toml.contains("features = [\"web-rocket\"]"));
     assert!(cargo_toml.contains("rocket = "));
 
-    let lib_file = read_to_string(temp_dir_path.join("src").join("lib.rs")).unwrap();
+    let lib_file = read_to_string(path.join("src").join("lib.rs")).unwrap();
     let expected = indoc! {r#"
     #[macro_use]
     extern crate rocket;
