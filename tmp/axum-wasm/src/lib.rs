61bd430b35f69f6f58c683fcc15d6f579b40da76
Cyndra-project <junaidgohan123@gmail.com>
2025-04-10 10:11:10 +0200

    feat: hook in runtime logs (#568)
    * feat: simple wasm log to send over unix socket
    
    * feat: hook up logs subscription
    
    * refactor: extra comments
    
    * refactor: simpler interface
    
    * refactor: combine JsonVisitors
    
    * refactor: clippy suggestions
    
    * refactor: update tests
    
    * refactor: extra comment
    
    * refactor: increase channel for better performance
    
    * refactor: move off stderr

--- tmp/axum-wasm/src/lib.rs
+++ tmp/axum-wasm/src/lib.rs
@@ -1,11 +1,15 @@
+use tracing::debug;
+
 klyra_codegen::app! {
     #[klyra_codegen::endpoint(method = get, route = "/hello")]
     async fn hello() -> &'static str {
+        debug!("called hello()");
         "Hello, World!"
     }
 
     #[klyra_codegen::endpoint(method = get, route = "/goodbye")]
     async fn goodbye() -> &'static str {
+        debug!("called goodbye()");
         "Goodbye, World!"
     }
 }
