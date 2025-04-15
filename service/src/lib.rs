b693d31f958572e52d8c09505736b70544d4498a
Cyndra-project <junaidgohan123@gmail.com>
2025-04-15 23:28:51 +0000

    Chore: v0.10.0 (#625)
    * chore: v0.10.0
    
    * chore: resources v0.10.0
    
    * misc: v0.10.0

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -27,7 +27,7 @@
 //! be a library crate with a `klyra-service` dependency with the `web-rocket` feature on the `klyra-service` dependency.
 //!
 //! ```toml
-//! klyra-service = { version = "0.9.0", features = ["web-rocket"] }
+//! klyra-service = { version = "0.10.0", features = ["web-rocket"] }
 //! ```
 //!
 //! A boilerplate code for your rocket project can also be found in `src/lib.rs`:
@@ -108,7 +108,7 @@
 //! Add `klyra-shared-db` as a dependency with the `postgres` feature, and add `sqlx` as a dependency with the `runtime-tokio-native-tls` and `postgres` features inside `Cargo.toml`:
 //!
 //! ```toml
-//! klyra-shared-db = { version = "0.9.0", features = ["postgres"] }
+//! klyra-shared-db = { version = "0.10.0", features = ["postgres"] }
 //! sqlx = { version = "0.6.2", features = ["runtime-tokio-native-tls", "postgres"] }
 //! ```
 //!
