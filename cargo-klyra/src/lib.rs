3ee87e45e94796ddaa822604d555726caf675e82
Cyndra-project <junaidgohan123@gmail.com>
2025-04-20 03:26:39 +0000

    ci: resolve CI errors in klyra-next (#580)
    * test: compile wasm module in axum runtime test setup
    
    * ci: add next patch override to CI
    
    * ci: include wasm32-wasi target in rust install
    
    * fix: deployer tests where runtime fails to start
    
    * fix: incorrect provisioner address
    
    * feat: log service state changes in runtime
    
    * feat: don't send stop req on startup failure
    
    * refactor: unused imports
    
    * refactor: handling legacy panics
    
    * tests: deadlock less
    
    * refactor: fixups
    
    * refactor: clippy suggestions
    
    * tests: mock provisioner
    
    * refactor: restore capture from 'log' and colors
    
    * refactor: clippy suggestions
    
    * tests: longer wait
    
    * tests: don't panic while holding lock
    
    * tests: don't panic on stream closed
    
    * tests: don't filter out state logs
    
    * tests: bigger timeout
    
    * ci: remove duplicate patch
    
    * refactor: comments
    
    ---------

--- cargo-klyra/src/lib.rs
+++ cargo-klyra/src/lib.rs
@@ -510,7 +510,7 @@ impl Klyra {
             .into_inner();
 
         tokio::spawn(async move {
-            while let Some(log) = stream.message().await.expect("to get log from stream") {
+            while let Ok(Some(log)) = stream.message().await {
                 let log: klyra_common::LogItem = log.try_into().expect("to convert log");
                 println!("{log}");
             }
