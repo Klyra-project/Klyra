68c627cdbc9270dc29bc66106504d4dbe4257312
Cyndra-project <junaidgohan123@gmail.com>
2025-05-07 13:00:54 +0200

    chore: move codegen::main from service to runtime (#1013)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -20,7 +20,7 @@
 //!
 //! Now that klyra is installed, you can initialize a project with Axum boilerplate:
 //! ```bash
-//! $ cargo klyra init --axum my-axum-app
+//! $ cargo klyra init --template axum my-axum-app
 //! ```
 //!
 //! By looking at the `Cargo.toml` file of the generated `my-axum-app` project you will see it has been made to
@@ -106,7 +106,7 @@
 //!
 //! Initialize a project with Rocket boilerplate:
 //! ```bash
-//! $ cargo klyra init --rocket my-rocket-app
+//! $ cargo klyra init --template rocket my-rocket-app
 //! ```
 //!
 //! Add `klyra-shared-db` as a dependency with the `postgres` feature, and add `sqlx` as a dependency with the
@@ -214,6 +214,59 @@
 //!
 //! You can also [open an issue or a discussion on GitHub](https://github.com/klyra-hq/klyra).
 //!
+
+/// Helper macro that generates the entrypoint required by any service - likely the only macro you need in this crate.
+///
+/// # Without klyra managed resources
+/// The simplest usage is when your service does not require any klyra managed resources, so you only need to return a klyra supported service:
+///
+/// ```rust,no_run
+/// use klyra_rocket::KlyraRocket;
+///
+/// #[klyra_rocket::main]
+/// async fn rocket() -> KlyraRocket {
+///     let rocket = rocket::build();
+///
+///     Ok(rocket.into())
+/// }
+/// ```
+///
+/// ## klyra supported services
+/// The following types can be returned from a `#[klyra_service::main]` function and enjoy first class service support in klyra.
+///
+/// | Return type                           | Crate                                                         | Service                                     | Version    | Example                                                                               |
+/// | ------------------------------------- |-------------------------------------------------------------- | ------------------------------------------- | ---------- | -----------------------------------------------------------------------------------   |
+/// | `KlyraActixWeb`                     |[klyra-actix-web](https://crates.io/crates/klyra-actix-web)| [actix-web](https://docs.rs/actix-web/4.3)  | 4.3        | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/actix-web/hello-world)      |
+/// | `KlyraAxum`                         |[klyra-axum](https://crates.io/crates/klyra-axum)          | [axum](https://docs.rs/axum/0.6)            | 0.5        | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/axum/hello-world)           |
+/// | `KlyraPoem`                         |[klyra-poem](https://crates.io/crates/klyra-poem)          | [poem](https://docs.rs/poem/1.3)            | 1.3        | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/poem/hello-world)           |
+/// | `KlyraPoise`                        |[klyra-poise](https://crates.io/crates/klyra-poise)        | [poise](https://docs.rs/poise/0.5)          | 0.5        | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/poise/hello-world)          |
+/// | `KlyraRocket`                       |[klyra-rocket](https://crates.io/crates/klyra-rocket)      | [rocket](https://docs.rs/rocket/0.5.0-rc.2) | 0.5.0-rc.2 | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/rocket/hello-world)         |
+/// | `KlyraSalvo`                        |[klyra-salvo](https://crates.io/crates/klyra-salvo)        | [salvo](https://docs.rs/salvo/0.37)         | 0.37       | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/salvo/hello-world)          |
+/// | `KlyraSerenity`                     |[klyra-serenity](https://crates.io/crates/klyra-serenity   | [serenity](https://docs.rs/serenity/0.11)   | 0.11       | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/serenity/hello-world)       |
+/// | `KlyraThruster`                     |[klyra-thruster](https://crates.io/crates/klyra-thruster)  | [thruster](https://docs.rs/thruster/1.3)    | 1.3        | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/thruster/hello-world)       |
+/// | `KlyraTower`                        |[klyra-tower](https://crates.io/crates/klyra-tower)        | [tower](https://docs.rs/tower/0.4)          | 0.4        | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/tower/hello-world)          |
+/// | `KlyraTide`                         |[klyra-tide](https://crates.io/crates/klyra-tide)          | [tide](https://docs.rs/tide/0.16)           | 0.16       | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/tide/hello-world)           |
+///
+/// # Getting klyra managed resources
+/// Klyra is able to manage resource dependencies for you. These resources are passed in as inputs to your `#[klyra_runtime::main]` function and are configured using attributes:
+/// ```rust,no_run
+/// use sqlx::PgPool;
+/// use klyra_rocket::KlyraRocket;
+///
+/// struct MyState(PgPool);
+///
+/// #[klyra_runtime::main]
+/// async fn rocket(#[klyra_shared_db::Postgres] pool: PgPool) -> KlyraRocket {
+///     let state = MyState(pool);
+///     let rocket = rocket::build().manage(state);
+///
+///     Ok(rocket.into())
+/// }
+/// ```
+///
+/// More [klyra managed resources can be found here](https://github.com/klyra-hq/klyra/tree/main/resources)
+pub use klyra_codegen::main;
+
 mod alpha;
 mod args;
 mod logger;
@@ -223,14 +276,15 @@ mod provisioner_factory;
 mod resource_tracker;
 
 pub use alpha::{start, Alpha};
-pub use async_trait::async_trait;
 pub use logger::Logger;
 #[cfg(feature = "next")]
 pub use next::{AxumWasm, NextArgs};
 pub use provisioner_factory::ProvisionerFactory;
 pub use resource_tracker::{get_resource, ResourceTracker};
 pub use klyra_common::storage_manager::StorageManager;
-pub use klyra_service::{main, CustomError, Error, Factory, ResourceBuilder, Service};
+pub use klyra_service::{CustomError, Error, Factory, ResourceBuilder, Service};
+
+pub use async_trait::async_trait;
 
 // Dependencies required by the codegen
 pub use anyhow::Context;
