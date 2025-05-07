bab0ea375d031ee7ef42cfa1eee1651b4d3134d8
Cyndra-project <junaidgohan123@gmail.com>
2025-05-07 13:00:14 +0200

    fix(runtime): Remove 2s startup sleep (#1012)
    * dont upload .git
    
    * Remove the 2 second sleep at runtime startup + logging and docs
    
    * some markdown formatting
    
    * move .git ignore (pun intended) to other PR

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -112,8 +112,9 @@ pub trait Factory: Send + Sync {
 /// Your resource will be available on a [klyra_runtime::main][main] function as follow:
 /// ```
 /// #[klyra_runtime::main]
-/// async fn my_service([custom_resource_crate::namespace::B] custom_resource: T)
-///     -> klyra_axum::KlyraAxum {}
+/// async fn my_service(
+///     [custom_resource_crate::namespace::B] custom_resource: T,
+/// ) -> klyra_axum::KlyraAxum {}
 /// ```
 ///
 /// Here `custom_resource_crate::namespace` is the crate and namespace to a builder `B` that implements [`ResourceBuilder`] to create resource `T`.
@@ -155,7 +156,6 @@ pub trait Factory: Send + Sync {
 ///         &self
 ///     }
 ///
-///
 ///     async fn output(self, factory: &mut dyn Factory) -> Result<Self::Output, klyra_service::Error> {
 ///         Ok(self.name)
 ///     }
@@ -171,8 +171,7 @@ pub trait Factory: Send + Sync {
 /// #[klyra_runtime::main]
 /// async fn my_service(
 ///     [custom_resource_crate::Builder(name = "John")] resource: custom_resource_crate::Resource
-/// )
-///     -> klyra_axum::KlyraAxum {}
+/// ) -> klyra_axum::KlyraAxum {}
 /// ```
 #[async_trait]
 pub trait ResourceBuilder<T> {
