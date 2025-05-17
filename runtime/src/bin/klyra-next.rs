0e12867cecc44a422141f04a38faf8a078d48758
Cyndra-project <junaidgohan123@gmail.com>
2025-05-17 20:31:58 +0300

    logger: adjust logger to receive logs blobs (#1172)
    * codegen: removed the otel layer generation
    
    * proto: updated logger proto file
    
    * logger: add store_logs RPC impl
    
    * logger: fixed tests
    
    * logger: decreased the visibility for Log type
    
    * logger: remove the unwrap
    
    * proto/logger: differentiate between a fetched/stored log items
    
    * logger: simplify tests
    
    * runtime: removed tracing/otel dependencies
    
    * runtime: removed the tracing dependencies
    
    * codegen: cleanup tracing layers and tests
    
    * runtime/next: fix compilation
    
    * logger: remove claim verification on store_logs
    
    * logger: implemented from_stored for StoredLogItem convertion to Log
    
    * codegen: readd a missing import
    
    * codegen: fix tests
    
    * addressed P review
    
    * logger: removed integration test comment
    
    * logger/tests: remove the DeploymentPush claim scope
    
    * address orhun feedback
    
    * logger: fixed error message typo
    
    * address Johan review
    
    * logger/proto: remove dedup protobuf defs

--- runtime/src/bin/klyra-next.rs
+++ runtime/src/bin/klyra-next.rs
@@ -3,7 +3,7 @@ use std::{
     time::Duration,
 };
 
-use klyra_common::backends::tracing::{setup_tracing, ExtractPropagationLayer};
+use klyra_common::backends::tracing::ExtractPropagationLayer;
 use klyra_proto::runtime::runtime_server::RuntimeServer;
 use klyra_runtime::{print_version, AxumWasm, NextArgs};
 use tonic::transport::Server;
@@ -19,8 +19,6 @@ async fn main() {
     }
 
     let args = NextArgs::parse().unwrap();
-
-    setup_tracing(tracing_subscriber::registry(), "klyra-next");
 
     trace!(args = ?args, "parsed args");
 
