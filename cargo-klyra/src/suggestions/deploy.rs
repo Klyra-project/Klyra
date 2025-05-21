f36443d6e0a030cf2873e7c4adb4f6c2fb8b117a
Cyndra-project <junaidgohan123@gmail.com>
2025-05-21 22:26:47 +0300

    feat(cargo-klyra): add suggestions in case of cmd failures (#1245)
    * feat(cargo-klyra): add suggestions in case of cmd failures
    
    * refactor(cargo-klyra): break suggestions module

---
+++ cargo-klyra/src/suggestions/deploy.rs
@@ -0,0 +1,52 @@
+use crossterm::style::Stylize;
+
+/// Used when the deploy request doesn't succeed.
+pub fn deploy_request_failure(err: anyhow::Error) -> anyhow::Error {
+    println!();
+    println!("{}", "Deploy request failed".red());
+    println!();
+    println!("Please check your project status and deployments:");
+    println!();
+    println!("1. cargo klyra project status");
+    println!();
+    println!("2. cargo klyra deployment list");
+    println!();
+    println!(
+        "If deploying fails repeteadly, please try restarting your project before deploying again or contacting the team on the Discord server:"
+    );
+    println!();
+    println!("cargo klyra project restart");
+    err
+}
+
+/// Especially used for cases where the deployment fails after the
+/// deploy request went through (e.g. following the deployment logs, checking
+/// the deployment state).
+pub fn deployment_setup_failure(err: anyhow::Error, title: &str) -> anyhow::Error {
+    println!();
+    println!("{}", title.dark_red());
+    println!();
+    println!(
+        "Please check your project status and if the last deployment is recent and is running:"
+    );
+    println!();
+    println!("1. cargo klyra project status");
+    println!();
+    println!("2. cargo klyra deployment list");
+    println!();
+    println!("You should be able to get the logs of the deployment by running:");
+    println!();
+    println!("cargo klyra logs");
+    println!();
+    println!("Or follow the logs of the deployment by running:");
+    println!();
+    println!("cargo klyra logs --follow");
+    println!("If the last deployment is not recent or is not running, please try deploying again  or contacting the team on the Discord server:");
+    println!();
+    println!("cargo klyra deploy");
+    println!();
+    println!("Or restart the project before deploying again:");
+    println!();
+    println!("cargo klyra project restart");
+    err
+}
---
+++ cargo-klyra/src/suggestions/logs.rs
@@ -0,0 +1,21 @@
+use crossterm::style::Stylize;
+
+/// Used to handle the case of getting the last deployment or getting
+/// the logs failed.
+pub fn get_logs_failure(err: anyhow::Error, title: &str) -> anyhow::Error {
+    println!();
+    println!("{}", title.red());
+    println!();
+    println!("Please check your project status and deployments:");
+    println!();
+    println!("1. cargo klyra project status");
+    println!();
+    println!("2. cargo klyra deployment list");
+    println!();
+    println!(
+        "If getting the logs fails repeteadly, please try restarting your project before getting the logs again or contacting the team on the Discord server:"
+    );
+    println!();
+    println!("cargo klyra project restart");
+    err
+}
