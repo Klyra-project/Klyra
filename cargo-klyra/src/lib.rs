9cf97b0f6b0de7a3ddb380e637a47a9ce54623fa
Cyndra-project <junaidgohan123@gmail.com>
2025-06-01 10:49:17 +0100

    fix(cargo-klyra): cargo klyra clean response type (#1409)

--- cargo-klyra/src/lib.rs
+++ cargo-klyra/src/lib.rs
@@ -635,7 +635,7 @@ impl Klyra {
 
     async fn clean(&self) -> Result<CommandOutcome> {
         let client = self.client.as_ref().unwrap();
-        let lines = client
+        let message = client
             .clean_project(self.ctx.project_name())
             .await
             .map_err(|err| {
@@ -646,12 +646,7 @@ impl Klyra {
                     "cleaning your project or checking its status fail repeatedly",
                 )
             })?;
-
-        for line in lines {
-            println!("{line}");
-        }
-
-        println!("Cleaning done!");
+        println!("{message}");
 
         Ok(CommandOutcome::Ok)
     }
