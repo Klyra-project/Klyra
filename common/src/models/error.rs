4b68dc028e8a3c50a3b8e1e0d259d350baa00bfc
Cyndra-project <junaidgohan123@gmail.com>
2025-06-02 09:59:37 +0000

    bug: remove project resources automatically when delete the project (#1421)
    * tests: deleting a project that has resources
    
    * feat: remove resources when deleting project
    
    * tests: deleting a project when resource removal fails
    
    * refactor: better error message
    
    * refactor: don't panic if project is not deleted
    
    * refactor: remove unneeded `Result` return type
    
    * refactor: helpful comment
    
    * refactor: clippy errors
    
    * tests: delete a project with a running deployment
    
    * feat: auto stop running deployment when deleting project
    
    * refactor: make gateway calls to project structured
    
    * refactor: reach out to support error message
    
    * refactor: remove dry-run option
    
    * refactor: handle deployments that might be in the building state
    
    * refactor: less `if` nesting
    
    * refactor: don't delete twice
    
    * refactor: clippy suggestion
    
    * refactor: more clippy errors
    
    * refactor: better default handling

--- common/src/models/error.rs
+++ common/src/models/error.rs
@@ -51,6 +51,7 @@ pub enum ErrorKind {
     TooManyProjects,
     ProjectHasResources(Vec<String>),
     ProjectHasRunningDeployment,
+    ProjectHasBuildingDeployment,
     CustomDomainNotFound,
     InvalidCustomDomain,
     CustomDomainAlreadyExists,
@@ -91,14 +92,18 @@ impl From<ErrorKind> for ApiError {
                 (StatusCode::FORBIDDEN, "You cannot create more projects. Delete some projects first.")
             },
             ErrorKind::ProjectHasRunningDeployment => (
-                StatusCode::FORBIDDEN,
-                "A deployment is running. Stop it with `cargo klyra stop` first."
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Could not automatically stop the running deployment for the project. Please reach out to Klyra support for help."
+            ),
+            ErrorKind::ProjectHasBuildingDeployment => (
+                StatusCode::BAD_REQUEST,
+                "Project currently has a deployment that is busy building. Use `cargo klyra deployment list` to see it and wait for it to finish"
             ),
             ErrorKind::ProjectHasResources(resources) => {
                 let resources = resources.join(", ");
                 return Self {
-                    message: format!("Project has resources: {}. Use `cargo klyra resource list` and `cargo klyra resource delete <type>` to delete them.", resources),
-                    status_code: StatusCode::FORBIDDEN.as_u16(),
+                    message: format!("Could not automatically delete the following resources: {}. Please reach out to Klyra support for help.", resources),
+                    status_code: StatusCode::INTERNAL_SERVER_ERROR.as_u16(),
                 }
             }
             ErrorKind::InvalidProjectName(err) => {
