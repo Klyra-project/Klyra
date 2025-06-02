b35ab1c9e18edc8c5d2ad8e46040f6ed1c7bb30a
Cyndra-project <junaidgohan123@gmail.com>
2025-06-02 17:34:34 +0100

    feat(klyra-axum): support axum 0.7 through feature flag (#1440)

--- services/klyra-axum/src/lib.rs
+++ services/klyra-axum/src/lib.rs
@@ -19,25 +19,40 @@
 use klyra_runtime::{CustomError, Error};
 use std::net::SocketAddr;
 
+#[cfg(feature = "axum")]
+use axum::Router;
+#[cfg(feature = "axum-0-7")]
+use axum_0_7::Router;
+
 /// A wrapper type for [axum::Router] so we can implement [klyra_runtime::Service] for it.
-pub struct AxumService(pub axum::Router);
+pub struct AxumService(pub Router);
 
 #[klyra_runtime::async_trait]
 impl klyra_runtime::Service for AxumService {
     /// Takes the router that is returned by the user in their [klyra_runtime::main] function
     /// and binds to an address passed in by klyra.
     async fn bind(mut self, addr: SocketAddr) -> Result<(), Error> {
+        #[cfg(feature = "axum")]
         axum::Server::bind(&addr)
             .serve(self.0.into_make_service())
             .await
             .map_err(CustomError::new)?;
+        #[cfg(feature = "axum-0-7")]
+        axum_0_7::serve(
+            klyra_runtime::tokio::net::TcpListener::bind(addr)
+                .await
+                .map_err(CustomError::new)?,
+            self.0,
+        )
+        .await
+        .map_err(CustomError::new)?;
 
         Ok(())
     }
 }
 
-impl From<axum::Router> for AxumService {
-    fn from(router: axum::Router) -> Self {
+impl From<Router> for AxumService {
+    fn from(router: Router) -> Self {
         Self(router)
     }
 }
