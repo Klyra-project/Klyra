1a83b533cdbc7c18cbf8f98056d09aa926f7b4cf
Cyndra-project <junaidgohan123@gmail.com>
2025-06-02 17:34:16 +0100

    fix(klyra-serenity): support serenity 0.12 through feature flag (#1439)

--- services/klyra-serenity/src/lib.rs
+++ services/klyra-serenity/src/lib.rs
@@ -54,8 +54,13 @@
 use klyra_runtime::{CustomError, Error};
 use std::net::SocketAddr;
 
+#[cfg(feature = "serenity")]
+use serenity::Client;
+#[cfg(feature = "serenity-0-12")]
+use serenity_0_12::Client;
+
 /// A wrapper type for [serenity::Client] so we can implement [klyra_runtime::Service] for it.
-pub struct SerenityService(pub serenity::Client);
+pub struct SerenityService(pub Client);
 
 #[klyra_runtime::async_trait]
 impl klyra_runtime::Service for SerenityService {
@@ -68,8 +73,8 @@ impl klyra_runtime::Service for SerenityService {
     }
 }
 
-impl From<serenity::Client> for SerenityService {
-    fn from(router: serenity::Client) -> Self {
+impl From<Client> for SerenityService {
+    fn from(router: Client) -> Self {
         Self(router)
     }
 }
