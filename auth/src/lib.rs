058c90dcb9f1225586343eb45f08cad56f332fa8
Cyndra-project <junaidgohan123@gmail.com>
2025-06-19 00:28:57 +0100

    fix(auth): user query columns (#1669)
    * fix(auth): user query columns
    
    * clippy

--- auth/src/lib.rs
+++ auth/src/lib.rs
@@ -10,7 +10,7 @@ use args::StartArgs;
 use klyra_common::{claims::AccountTier, ApiKey};
 use sqlx::{migrate::Migrator, query, PgPool};
 use tracing::info;
-use user::User;
+pub use user::User;
 
 use crate::api::serve;
 pub use api::ApiBuilder;
