cf701043c4ca8ec2de8d76b65fda0b934c09b9ff
Cyndra-project <junaidgohan123@gmail.com>
2025-06-23 21:56:42 +0200

    feat: add resource request struct to backends (#1718)

--- backends/src/headers.rs
+++ backends/src/headers.rs
@@ -33,6 +33,39 @@ impl Header for XKlyraAdminSecret {
     }
 }
 
+pub static X_klyra_PROJECT_SECRET: HeaderName =
+    HeaderName::from_static("x-klyra-project-secret");
+
+/// Typed header for sending admin secrets to Klyra components
+pub struct XKlyraProjectSecret(pub String);
+
+impl Header for XKlyraProjectSecret {
+    fn name() -> &'static HeaderName {
+        &X_klyra_PROJECT_SECRET
+    }
+
+    fn decode<'i, I>(values: &mut I) -> Result<Self, headers::Error>
+    where
+        Self: Sized,
+        I: Iterator<Item = &'i http::HeaderValue>,
+    {
+        let value = values
+            .next()
+            .ok_or_else(headers::Error::invalid)?
+            .to_str()
+            .map_err(|_| headers::Error::invalid())?
+            .to_string();
+
+        Ok(Self(value))
+    }
+
+    fn encode<E: Extend<http::HeaderValue>>(&self, values: &mut E) {
+        if let Ok(value) = HeaderValue::from_str(&self.0) {
+            values.extend(std::iter::once(value));
+        }
+    }
+}
+
 /// Used by deployers <=0.38.0. Can be removed when those are no longer supported
 pub static X_klyra_PROJECT: HeaderName = HeaderName::from_static("x-klyra-project");
 
