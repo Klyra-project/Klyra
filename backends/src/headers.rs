926c857f8ef32f21f8b9213a4d38b9280d6239e0
Cyndra-project <junaidgohan123@gmail.com>
2025-06-24 21:30:25 +0200

    feat: add models and header for new ecs provisioner (#1730)

--- backends/src/headers.rs
+++ backends/src/headers.rs
@@ -66,6 +66,38 @@ impl Header for XKlyraProjectSecret {
     }
 }
 
+/// Used to ensure requests originate from the control service.
+pub static X_klyra_CTL_SECRET: HeaderName = HeaderName::from_static("x-klyra-ctl-secret");
+
+pub struct XKlyraCtlSecret(pub String);
+
+impl Header for XKlyraCtlSecret {
+    fn name() -> &'static HeaderName {
+        &X_klyra_CTL_SECRET
+    }
+
+    fn decode<'i, I>(values: &mut I) -> Result<Self, headers::Error>
+    where
+        Self: Sized,
+        I: Iterator<Item = &'i HeaderValue>,
+    {
+        let value = values
+            .next()
+            .ok_or_else(headers::Error::invalid)?
+            .to_str()
+            .map_err(|_| headers::Error::invalid())?
+            .to_string();
+
+        Ok(Self(value))
+    }
+
+    fn encode<E: Extend<HeaderValue>>(&self, values: &mut E) {
+        if let Ok(value) = HeaderValue::from_str(self.0.as_str()) {
+            values.extend(std::iter::once(value));
+        }
+    }
+}
+
 /// Used by deployers <=0.38.0. Can be removed when those are no longer supported
 pub static X_klyra_PROJECT: HeaderName = HeaderName::from_static("x-klyra-project");
 
