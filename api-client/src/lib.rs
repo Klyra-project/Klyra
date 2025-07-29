a95863b02746beedff07f29988c6328d239deba2
Cyndra-project <junaidgohan123@gmail.com>
2025-07-29 02:26:35 +0100

    fix(api-client): update archive endpoint (#1914)

--- api-client/src/lib.rs
+++ api-client/src/lib.rs
@@ -149,7 +149,7 @@ impl KlyraApiClient {
         project: &str,
         data: Vec<u8>,
     ) -> Result<UploadArchiveResponseBeta> {
-        let path = format!("/projects/{project}/deployments/archives");
+        let path = format!("/projects/{project}/archives");
 
         let url = format!("{}{}", self.api_url, path);
         let mut builder = self.client.post(url);
