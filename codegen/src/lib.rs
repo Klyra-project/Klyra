0d0f3c2145aa61d350312faa1177cd3befd41246
Cyndra-project <junaidgohan123@gmail.com>
2025-08-16 21:48:49 +0100

    feat: team models, team project lists (#1986)
    * feat(common): team models
    
    * nit: don't print auth0 id
    
    * nit: make project delete prompt show project name
    
    * feat: update project new options
    
    * feat: add team member
    
    * account tier: add growth, remove deployer
    
    * fix: codegen docs
    
    * docs/typos
    
    * show feature flags in account output
    
    * show team in proj list and link
    
    * show team in proj stat
    
    * don't show deployment status in proj stat
    
    * clippy

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -8,7 +8,7 @@ mod klyra_main;
 /// ```rust,no_run
 /// use klyra_rocket::KlyraRocket;
 ///
-/// #[klyra_rocket::main]
+/// #[klyra_runtime::main]
 /// async fn rocket() -> KlyraRocket {
 ///     let rocket = rocket::build();
 ///
@@ -17,19 +17,19 @@ mod klyra_main;
 /// ```
 ///
 /// ## Klyra supported services
-/// The following types can be returned from a `#[klyra_service::main]` function and enjoy first class service support in klyra.
+/// The following types can be returned from a `#[klyra_runtime::main]` function and enjoy first class service support in klyra.
 ///
-/// | Return type       | Crate                                                          | Service                                                                          | Version    | Example                                                                                 |
-/// | ----------------- | -------------------------------------------------------------- | -------------------------------------------------------------------------------- | ---------- | --------------------------------------------------------------------------------------- |
-/// | `KlyraActixWeb` | [klyra-actix-web](https://crates.io/crates/klyra-actix-web)| [actix-web](https://docs.rs/actix-web/4.3)                                       | 4.3        | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/actix-web/hello-world)|
-/// | `KlyraAxum`     | [klyra-axum](https://crates.io/crates/klyra-axum)          | [axum](https://docs.rs/axum/0.7)                                                 | 0.7        | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/axum/hello-world)     |
-/// | `KlyraPoem`     | [klyra-poem](https://crates.io/crates/klyra-poem)          | [poem](https://docs.rs/poem/2.0)                                                 | 2.0        | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/poem/hello-world)     |
-/// | `KlyraRocket`   | [klyra-rocket](https://crates.io/crates/klyra-rocket)      | [rocket](https://docs.rs/rocket/0.5)                                             | 0.5        | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/rocket/hello-world)   |
-/// | `KlyraSalvo`    | [klyra-salvo](https://crates.io/crates/klyra-salvo)        | [salvo](https://docs.rs/salvo/0.63)                                              | 0.63       | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/salvo/hello-world)    |
-/// | `KlyraSerenity` | [klyra-serenity](https://crates.io/crates/klyra-serenity)  | [serenity](https://docs.rs/serenity/0.12) and [poise](https://docs.rs/poise/0.6) | 0.12       | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/serenity/hello-world) |
-/// | `KlyraThruster` | [klyra-thruster](https://crates.io/crates/klyra-thruster)  | [thruster](https://docs.rs/thruster/1.3)                                         | 1.3        | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/thruster/hello-world) |
-/// | `KlyraTower`    | [klyra-tower](https://crates.io/crates/klyra-tower)        | [tower](https://docs.rs/tower/0.4)                                               | 0.4        | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/tower/hello-world)    |
-/// | `KlyraTide`     | [klyra-tide](https://crates.io/crates/klyra-tide)          | [tide](https://docs.rs/tide/0.16)                                                | 0.16       | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/tide/hello-world)     |
+/// | Return type       | Crate                                                          | Service                                                                 | Example                                                                                 |
+/// | ----------------- | -------------------------------------------------------------- | ----------------------------------------------------------------------- | --------------------------------------------------------------------------------------- |
+/// | `KlyraActixWeb` | [klyra-actix-web](https://crates.io/crates/klyra-actix-web)| [actix-web](https://docs.rs/actix-web)                                  | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/actix-web/hello-world)|
+/// | `KlyraAxum`     | [klyra-axum](https://crates.io/crates/klyra-axum)          | [axum](https://docs.rs/axum)                                            | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/axum/hello-world)     |
+/// | `KlyraPoem`     | [klyra-poem](https://crates.io/crates/klyra-poem)          | [poem](https://docs.rs/poem)                                            | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/poem/hello-world)     |
+/// | `KlyraRocket`   | [klyra-rocket](https://crates.io/crates/klyra-rocket)      | [rocket](https://docs.rs/rocket)                                        | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/rocket/hello-world)   |
+/// | `KlyraSalvo`    | [klyra-salvo](https://crates.io/crates/klyra-salvo)        | [salvo](https://docs.rs/salvo)                                          | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/salvo/hello-world)    |
+/// | `KlyraSerenity` | [klyra-serenity](https://crates.io/crates/klyra-serenity)  | [serenity](https://docs.rs/serenity) and [poise](https://docs.rs/poise) | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/serenity/hello-world) |
+/// | `KlyraThruster` | [klyra-thruster](https://crates.io/crates/klyra-thruster)  | [thruster](https://docs.rs/thruster)                                    | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/thruster/hello-world) |
+/// | `KlyraTower`    | [klyra-tower](https://crates.io/crates/klyra-tower)        | [tower](https://docs.rs/tower)                                          | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/tower/hello-world)    |
+/// | `KlyraTide`     | [klyra-tide](https://crates.io/crates/klyra-tide)          | [tide](https://docs.rs/tide)                                            | [GitHub](https://github.com/klyra-hq/klyra-examples/tree/main/tide/hello-world)     |
 ///
 /// ## Getting klyra managed resources
 /// Klyra is able to manage resource dependencies for you. These resources are passed in as inputs to your `#[klyra_runtime::main]` function and are configured using attributes:
